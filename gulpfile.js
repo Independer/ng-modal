0c4fc760c99faedad8595780c8ac8f6ba1ff14fd
Pavlo Glazkov <p.glazkov@gmail.com>
2019-06-19 17:46:32 +0200

    Add initial support for `ng add`

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ const rootFolder = path.join(__dirname);
 const srcFolder = path.join(rootFolder, 'projects', 'ng-modal', 'src');
 const themesSrcFolder = path.join(srcFolder, 'lib', 'themes');
 const distFolder = path.join(rootFolder, 'dist', 'ng-modal');
+const schematicsDistFolder = path.join(distFolder, 'schematics');
 const themesDistFolder = path.join(distFolder, 'themes');
 
 const themes = readdirSync(themesSrcFolder);
@@ -37,6 +38,19 @@ gulp.task('copy:themes-src', function() {
     return gulp.src([`${themesSrcFolder}/${theme}/*.scss`, `${themesSrcFolder}/${theme}/assets/**/*.*`], {base: `${themesSrcFolder}/${theme}`})
       .pipe(gulp.dest(`${themesDistFolder}/${theme}/src`));
   }));
+
+});
+
+gulp.task('copy:schematics:assets', function() {
+  return gulp.src(
+    [
+      './projects/ng-modal/schematics/*/schema.json',
+      './projects/ng-modal/schematics/*/files/**',
+      './projects/ng-modal/schematics/collection.json'
+    ],
+    { base: './projects/ng-modal/schematics' }
+  )
+  .pipe(gulp.dest(schematicsDistFolder));
 
 });
 
